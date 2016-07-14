299bf630d5fd9315c0b90c329e71bd6cbab1d569
Alexander Kessler <alexander-kessler@web.de>
2016-07-14 14:21:09 +0200

    Revert "Revert "#""
    This reverts commit 844ca2f17c6c66a44040272333d1aa7e4a0a252e.

--- client/main/main.routes.ng.js
+++ client/main/main.routes.ng.js
@@ -4,8 +4,9 @@ angular.module('corkboardApp')
 .config(function($stateProvider) {
   $stateProvider
   .state('main', {
-    url: '/main',
+    url: '/',
     templateUrl: 'client/main/main.view.ng.html',
-    controller: 'MainCtrl'
+    controller: 'MainCtrl',
+    data: {requireLogin : true}
   });
 });
